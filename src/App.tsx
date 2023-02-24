d2dc35ad4c21c9af90204e52a0a80b00d9167bc9
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-24 18:46:31 -0300

    chore: create theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
-import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-	const [count, setCount] = useState(0);
-
-	return <h1>oi</h1>;
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<h1>oi</h1>
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 }
