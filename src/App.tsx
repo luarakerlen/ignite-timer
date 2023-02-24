a063e0ed69ab645184c7995aff8458281f69a8f9
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-24 19:41:00 -0300

    feat: create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { Router } from './Router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <h1>oi</h1>
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+	return (
+		<ThemeProvider theme={defaultTheme}>
+			<BrowserRouter>
+				<Router />
+			</BrowserRouter>
+			<GlobalStyle />
+		</ThemeProvider>
+	);
 }
