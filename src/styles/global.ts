c6dbbe5299a180af6e15de9e26de9490d7b5113a
Luara Kerlen dos Santos Rangel <26902816+luarakerlen@users.noreply.github.com>
2023-02-24 22:35:29 -0300

    refactor: update home page style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
 *{
@@ -15,11 +15,12 @@ export const GlobalStyle = createGlobalStyle`
 body {
   background: ${(props) => props.theme['gray-900']};
   color: ${(props) => props.theme['gray-300']};
+  -webkit-font-smoothing: antialiased;
 }
 
-border-style, input-security, textarea, button {
+body, input, textarea, button {
   font-family: 'Roboto', sans-serif;
   font-weight: 400;
   font-size: 1rem;
 }
-`
+`;
